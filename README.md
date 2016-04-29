13ac2f85cc77a0a4046a66e52d7390e8b486fbae
tomcek112 <thomas.lutzeyer@gmail.com>
2016-04-29 18:53:55 +0100

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Modal Remover
+Removes annoying, screen-blocking modals that force users to sign up, disable ad-block or join a newsletter.
---
+++ inject.js
@@ -0,0 +1,36 @@
+// copied with pride from http://stackoverflow.com/questions/1118198/how-can-you-figure-out-the-highest-z-index-in-your-document
+// thanks a lot to http://stackoverflow.com/users/136852/flo
+function findHighestZIndex(elem)
+{
+  var elems = document.getElementsByTagName(elem);
+  var highest = 0;
+  var ret;
+  for (var i = 0; i < elems.length; i++)
+  {
+    var zindex=document.defaultView.getComputedStyle(elems[i],null).getPropertyValue("z-index");
+    var obj = elems[i]
+    if ((zindex > highest) && (zindex != 'auto'))
+    {
+      highest = zindex;
+      ret = obj
+    }
+  }
+  return ret;
+}
+
+
+(function() {
+
+	var bla = findHighestZIndex('div');
+	bla.remove();
+	console.log(bla)
+
+
+
+})();
+
+
+
+
+
+
